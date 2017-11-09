13c7a6a36e6916e2cfbc2bb6b986df18925ead1e
pallavi Dhepe <pallavid@xpanxion.co.in>
2017-11-09 23:02:31 +0530

    Initial Checkin

--- POC_Azure_ReadWriteJSONInAzureStorageMessage/Program.cs
+++ POC_Azure_ReadWriteJSONInAzureStorageMessage/Program.cs
@@ -7,6 +7,8 @@ namespace POC_Azure_ReadWriteJSONInAzureStorageMessage
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
