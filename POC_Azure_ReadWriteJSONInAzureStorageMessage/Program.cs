d60995603cdb78e09a4a5a5dde518932614b1c53
pallavi Dhepe <pallavid@xpanxion.co.in>
2017-11-09 22:55:52 +0530

    Add project files.

---
+++ POC_Azure_ReadWriteJSONInAzureStorageMessage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POC_Azure_ReadWriteJSONInAzureStorageMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
