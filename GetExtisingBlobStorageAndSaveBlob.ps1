9be694f16dfe92612f240658ba952a23adace1b1
PallaviXP <24568150+PallaviXP@users.noreply.github.com>
2017-11-17 10:35:22 +0530

    Add files via upload

---
+++ GetExtisingBlobStorageAndSaveBlob.ps1
@@ -0,0 +1,21 @@
+﻿#howtoqueuestorage1  = storage account
+# myqueueresourcegroup = resource group
+
+
+Login-AzureRmAccount
+$account = Get-AzureRmStorageAccount -Name "howtoqueuestorage1" -ResourceGroupName "myqueueresourcegroup"
+$account
+$con = $account.Context
+
+#listout container named mycontainer
+Get-AzureStorageContainer -Name my* -Context $con
+
+#uploads the blob
+$containerName = "mycontainer"
+Set-AzureStorageBlobContent -File "D:\Personal\2.bmp" -Container $containerName -Blob "2" -Context $con
+
+#list out the blobs
+Get-AzureStorageBlob -Container $ContainerName -Context $ctx | select Name 
+
+#download the blob
+Get-AzureStorageBlobContent -Blob "2" -Container $containerName -Destination "d:\" -Context $con
